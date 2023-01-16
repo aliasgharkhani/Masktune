9a0e2414a713c8d11ddb1aa7469866b865cbd137
aliasgharkhani <khani.ali1376@gmail.com>
2023-01-16 07:53:39 -0800

    Update catsvsdogs_masktune.sh

--- bash_files/catsvsdogs_masktune.sh
+++ bash_files/catsvsdogs_masktune.sh
@@ -3,19 +3,19 @@ cd path/to/the/cloned/folder
 
 python3 -m src.main --dataset catsvsdogs \
                     --train \
-                    --arch vgg16_bn  \
+                    --arch resnet32  \
                     --base_dir path/to/the/cloned/folder \
-                    --lr 0.1 \
+                    --lr 0.001 \
                     --momentum 0.9 \
-                    --weight_decay 5e-4 \
-                    --gamma 0.5 \
-                    --schedule 25 50 75 100 125 150 175 200 225 250 275 \
+                    --weight_decay 1e-4 \
+                    --gamma 0.1 \
+                    --schedule 50 \
                     --use_cuda \
-                    --optimizer sgd \
-                    --train_batch 256 \
-                    --test_batch 256 \
-                    --masking_batch 256 \
-                    --epochs 300 \
+                    --optimizer adam \
+                    --train_batch 128 \
+                    --test_batch 128 \
+                    --masking_batch 128 \
+                    --epochs 200 \
                     --masktune \
                     --selective_classification \
                     --dataset_dir path/to/the/cloned/folder/ + datasets/catsvsdogs/raw
