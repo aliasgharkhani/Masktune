9223ca0c020bb2a9b5f6eafa3574a62f0c999900
aliasgharkhani <khani.ali1376@gmail.com>
2023-01-16 07:35:56 -0800

    Update cifar10_masktune.sh

--- bash_files/cifar10_masktune.sh
+++ bash_files/cifar10_masktune.sh
@@ -3,13 +3,13 @@ cd path/to/the/cloned/folder
 
 python3 -m src.main --dataset cifar10 \
                     --train \
-                    --arch vgg16_bn  \
+                    --arch resnet32  \
                     --base_dir path/to/the/cloned/folder \
                     --lr 0.1 \
                     --momentum 0.9 \
-                    --weight_decay 5e-4 \
+                    --weight_decay 1e-4 \
                     --gamma 0.5 \
-                    --schedule 50 100 150 200 250 \
+                    --schedule 25 50 75 100 125 150 175 200 225 250 275 \
                     --use_cuda \
                     --optimizer sgd \
                     --train_batch 128 \
